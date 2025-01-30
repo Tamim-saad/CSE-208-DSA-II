a10372ee89544fd754ae75983bd6bbf31d8213e8
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2025-01-30 20:08:56 +0600

    Update soln_Prims.cpp

--- MSpanningTree_1/soln_Prims.cpp
+++ MSpanningTree_1/soln_Prims.cpp
@@ -1,82 +1,68 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 const int N = 1e4 + 5;
-const int INF = 2e5 + 5;
+const ll INF = 2e18 + 5;
 
-int graph[N][N];
+vector<pair<ll, ll>> adj[N];
 
-void prims(int n) {
-    map<int, int> value;
-    map<int, int> parent;
-    map<int, bool> visited;
+void prims(ll n) {
+  vector<ll> value(n + 1, INF);
+  vector<ll> parent(n + 1, -1);
+  vector<bool> visited(n + 1, false);
 
-    for (int i = 1; i <= n; i++) {
-        value[i] = INF;
-        visited[i] = false;
+  priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+
+  pq.push({0, 1});
+  value[1] = 0;
+
+  while (!pq.empty()) {
+    ll curr = pq.top().second;
+    ll currWeight = pq.top().first;
+    pq.pop();
+
+    if (visited[curr])
+      continue;
+    visited[curr] = true;
+
+    for (auto neighbor : adj[curr]) {
+      ll nextNode = neighbor.first;
+      ll edgeWeight = neighbor.second;
+
+      if (!visited[nextNode] && edgeWeight < value[nextNode]) {
+        parent[nextNode] = curr;
+        value[nextNode] = edgeWeight;
+        pq.push({value[nextNode], nextNode});
+      }
     }
+  }
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  ll ans = 0;
+  for (ll i = 2; i <= n; i++) {
+    ans += value[i];
+  }
 
-    pq.push({0, 1});
-    parent[1] = -1;
-    value[1] = 0;
+  cout << ans << endl;
 
-    while (!pq.empty()) {
-        int curr = pq.top().second;
-        pq.pop();
-
-        if (visited[curr]) continue;
-        visited[curr] = true;
-
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i] && graph[curr][i] > 0 && graph[curr][i] < value[i]) {
-                parent[i] = curr;
-                value[i] = graph[curr][i];
-                pq.push({value[i], i});
-            }
-        }
-    }
-
-    int ans = 0;
-    for (int i = 2; i <= n; i++) {
-        ans += graph[i][parent[i]];
-    }
-
-    cout<<"Prims Algorithm:\nTotal weight = "<<ans<<endl;
-    cout<<"ROOT Node: "<<0<<endl;
-    for (int i = 2; i <= n; i++) {
-        cout << i - 1 << ' ' << parent[i] - 1 << '\n';
-    }
+  // cout << "Edges of MST:" << endl;
+  // for (ll i = 2; i <= n; i++) {
+  //   cout << parent[i] << " - " << i << " : " << value[i] << endl;
+  // }
 }
 
 int main() {
+  ll n, m, s;
+  cin >> n >> m;
 
-    ifstream infile("prims_input.txt");
-    ofstream outfile("prims_output.txt");
+  for (ll i = 0; i < m; i++) {
+    ll u, v, w;
+    cin >> u >> v >> w;
+    adj[u].push_back({v, w});
+    adj[v].push_back({u, w});
+  }
+  // cin >> s;
 
-
-    if(!infile)
-    {
-        cout<<"Could not open the Input File";
-        return 0;
-    }
-
-
-    int n, m;
-    infile >> n >> m;
-
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        infile >> u >> v >> w;
-        graph[u + 1][v + 1] = w;
-        graph[v + 1][u + 1] = w;
-    }
-
-    prims(n);
-
-    infile.close();
-    outfile.close();
-
-    return 0;
+  prims(n);
+  return 0;
 }
